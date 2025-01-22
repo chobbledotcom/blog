78fa4fe6184286faeb148db7d2195a00566595e6
Stefan <git@stefn.co.uk>
2025-01-22 12:56:22 +0000

    Fix sitemap, add robots.txt

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,6 +1,6 @@
 export default {
   title: "blog.chobble.com",
-  url: "https://blog.chobble.com/",
+  url: "https://blog.chobble.com",
   language: "en",
   description: "Nix/NixOS, self hosting, and owning your data",
 };
