c6873bf6ace6eab65ca8a53292d35c01d4273381
Stefan <git@stefn.co.uk>
2025-03-30 19:12:52 +0100

    Update flake and default.nix

--- default.nix
+++ default.nix
@@ -1,53 +1,14 @@
-{
-  pkgs ? import <nixpkgs> { },
-}:
-
-let
-  # Input source files
-  src = ./.;
-  nodeDeps = import ./node-deps.nix { inherit pkgs; };
-  inherit (nodeDeps) packageJSON nodeModules;
-
-in
-pkgs.stdenv.mkDerivation {
-  name = "blog-chobble-com";
-
-  src = builtins.filterSource (
-    path: type:
-    !(builtins.elem (baseNameOf path) [
-      "_site"
-      "node_modules"
-      ".git"
-    ])
-  ) src;
-
-  nativeBuildInputs = with pkgs; [
-    cacert
-    ubuntu-sans
-    minify
-    yarn
-  ];
-
-  configurePhase = ''
-    export HOME=$TMPDIR
-    mkdir -p _site/style
-
-    cp -r ${nodeModules}/node_modules .
-    chmod -R +w node_modules
-    cp ${packageJSON} package.json
-  '';
-
-  buildPhase = ''
-    echo 'Building site'
-    yarn --offline eleventy
-
-    echo 'Minifying HTML'
-    minify --all --recursive --output . _site
-  '';
-
-  installPhase = ''
-    mkdir -p $out
-    cp -r _site/* $out/
-    rm -rf node_modules _site package.json
-  '';
-}
+(import
+  (
+    let
+      flake-compat = fetchTarball {
+        url = "https://github.com/edolstra/flake-compat/archive/12c64ca55c1014cdc1b16ed5a804aa8576601ff2.tar.gz";
+        sha256 = "0jm6nzb83wa6ai17ly9fzpqc40wg1viib8klq8lby54agpl213w5";
+      };
+    in
+    "${flake-compat}/default.nix"
+  )
+  {
+    src = ./.;
+  }
+).defaultNix.packages.${builtins.currentSystem}.site
